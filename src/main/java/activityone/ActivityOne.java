2d385ed1b41ce3beae72275faabdf02ea52ee261
PC <PC@DESKTOP-IFFPGIN>
2023-09-26 18:00:15 -0700

    First Commit

---
+++ src/main/java/activityone/ActivityOne.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package activityone;
+
+/**
+ *
+ * @author PC
+ */
+public class ActivityOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Sample for GIT commit and push");
+    }
+}
